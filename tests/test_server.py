ee35c9d05f6082a3da597a906c738eaf6ab58f63
melonkernel <toffe@bskog.com>
2025-05-27 09:36:40 +0300

    fix ci

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,9 +1,6 @@
 import pytest
-from mcp_fmi.server import mcp, ping  # make sure you're importing the right mcp instance
+from mcp_fmi.server import mcp, fmu_information_tool  # make sure you're importing the right mcp instance
 
-@pytest.mark.asyncio
-async def test_ping_tool():
-    result = await mcp.call_tool("ping", {})
-    assert isinstance(result, list)
-    assert len(result) == 1
-    assert result[0].text == "pong"
+def test_fmu_information_tool_tool():
+    result = fmu_information_tool()
+    assert result is not None
